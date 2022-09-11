b17b8e5fd0643f895ffe064780b59e17c9f5bb3b
noorsiddiqui-prog <noorsiddiqui048@gmail.com>
2022-09-11 12:42:17 +0500

    updation

--- pages/admin-portal/Profile.js
+++ pages/admin-portal/Profile.js
@@ -1,9 +1,30 @@
 import ProfileComp from "../../components/admin_hotel/ProfileComp"
 import Sidebar from "../../components/admin_hotel/Sidebar"
 import React from 'react'
+import { useState, useEffect } from "react"
+import { getSession, signIn, SignIn } from "next-auth/react"
+
 
 export default function Profile() {
+  const [ loading, setLoading] = useState(true)
+  useEffect(() => {
+    const securePage = async() => {
+    const session = await getSession()
+    if (!session){
+      signIn()
+
+    }else {
+      setLoading(false)
+    }
+    }
+    securePage()
+  }, [])
+
+  if (loading){
+      <h2>loading ... !</h2>
+  }
   return (
+    
     <div className="flex">
       <Sidebar />
        <ProfileComp/>
