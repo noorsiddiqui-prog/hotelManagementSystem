b435044d27b5ad39cb1a98207a9bc5c0cfb9e57a
noorsiddiqui-prog <noorsiddiqui048@gmail.com>
2022-09-17 13:35:45 +0500

    17/9 updated get data with token

--- components/admin_hotel/RoomsListComp.js
+++ components/admin_hotel/RoomsListComp.js
@@ -155,22 +155,34 @@ useEffect(() => {
             </tr>
         </thead>
         <tbody>
+
+        {/* <div>
+    {loading && <div>Loading</div>}
+    {!loading && (
+      <div>
+        
+        {data.map(item => (<tr className='bg-sky-500/50 mb-20'><td>{item.beds}</td>
+        <td>{item.room}</td></tr>))}
+      </div>
+    )}
+    </div> */}
+
             {/* {
             dataFetch && dataFetch.map((x) => { 
                  
                  return(  */}
                 
-                {data && data.map((x) => {
+                {data.map(x => (
                     <div >
                  <tr  class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td class="p-4 w-4">
+                {/* <td class="p-4 w-4">
                     <div class="flex items-center">
                         <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                         <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
                     </div>
-                </td>
+                </td> */}
                 <th class=" w-4 h-4">
-                    <img src={x.room_images} />
+                    <img src={x.room_images} width={100}/>
                 </th>
                 <td scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     {x.room_type}
@@ -203,7 +215,7 @@ useEffect(() => {
             
             </tr>
             </div>
-})}
+))}
             {/* <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                 <td class="p-4 w-4">
                     <div class="flex items-center">
