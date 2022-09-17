5239ee9ac0afaddc06d5de5abd723ae00bc0a6ad
noorsiddiqui-prog <noorsiddiqui048@gmail.com>
2022-09-17 13:05:53 +0500

    17/9 updated get data with token

--- components/admin_hotel/RoomsListComp.js
+++ components/admin_hotel/RoomsListComp.js
@@ -1,6 +1,96 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+
+
+// export const getStaticProps = async () => {
+    const token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjYzNDA5NDk2LCJpYXQiOjE2NjMzOTg2OTYsImp0aSI6ImUzZWU2NjBhYjJkYzQ4M2Y5MDhmMjZkNTQ4NjU1ZTAzIiwidXNlcl9pZCI6M30.S2-RWyJMZzW8htHOqvrqQ6IrcKuA_oJOSYNBi4P_7Ec"
+
+//     const res = await fetch("http://127.0.0.1:8000/portal/room/", {
+//         headers: {
+//             // 'Accept': 'application/json',
+//             // 'Content-Type': 'multipart/form-data',
+//             'Authorization': `Bearer ${token}`
+//         }
+//     });
+//     const dataFetch = await res.json();
+    
+//     return {
+//         props:{
+//             dataFetch,
+//         }
+//     }
+// }
+
+
+
+
+
+
 
 export default function RoomsListComp() {
+
+    const [chars, setChars] = useState([]);
+
+// useEffect(() => {
+//     (async () => {
+//         const todos = await axios.get('http://127.0.0.1:8000/portal/room/', {
+//                     headers: {
+//                         // 'Accept': 'application/json',
+//                         // 'Content-Type': 'multipart/form-data',
+//                         'Authorization': `Bearer ${token}`
+//     }})
+
+//     const todosData = todos.data;
+//     setTodos(todosData)
+
+//     })();
+// }, [])
+
+
+// useEffect(() => {
+//     (async () => {
+//         const todos = await  axios({
+//             method: "get",
+//             url: 'http://127.0.0.1:8000/portal/room/list/',
+//             headers: {
+//               Authorization: `Bearer ${token}`,
+//             },
+//         }).then((response) => {
+//             console.log(response.data);
+//             const todosData = response.data
+//             setTodos(todosData)
+//           });
+
+
+
+//     })();
+// }, [])
+
+
+
+useEffect(() => {
+    (async () => {
+        const todos = await  axios({
+            method: "get",
+            url: 'http://127.0.0.1:8000/portal/room/list/',
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+        }).then((response) => {
+            console.log(response.data);
+            const todosData = response.data
+            setChars(todosData)
+          });
+
+
+
+    })();
+}, [])
+
+
+
+    
   return (
     <div>
       
@@ -21,19 +111,19 @@ export default function RoomsListComp() {
                     Room Type
                 </th>
                 <th scope="col" class="py-3 px-6">
-                    Room number
+                    Room Number
                 </th>
                 <th scope="col" class="py-3 px-6">
                     Beds
                 </th>
                 <th scope="col" class="py-3 px-6">
-                    Price per Person
+                    Price
                 </th>
                 <th scope="col" class="py-3 px-6">
                     Available
                 </th>
                 <th scope="col" class="py-3 px-6">
-                    Booked
+                    Hotel No
                 </th>
                 <th scope="col" class="py-3 px-6">
                     Description
@@ -44,7 +134,14 @@ export default function RoomsListComp() {
             </tr>
         </thead>
         <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            {/* {
+            dataFetch && dataFetch.map((x) => { 
+                 
+                 return(  */}
+                
+                {chars && chars.map((x) => {
+                    <div>
+                 <tr  class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                 <td class="p-4 w-4">
                     <div class="flex items-center">
                         <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
@@ -52,29 +149,30 @@ export default function RoomsListComp() {
                     </div>
                 </td>
                 <th class=" w-4 h-4">
-                    <img src="https://images.unsplash.com/photo-1574484284002-952d92456975?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bXV0dG9uJTIwY3Vycnl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
+                    {/* <img src={x.room_images} /> */}
                 </th>
                 <td scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
+                    {/* {x.room_type} */}
                 </td>
                 <td class="py-4 px-6">
-                    Sliver
+                    {chars.room}
                 </td>
                 <td class="py-4 px-6">
-                    Laptop
+                    {/* {x.beds} */}
                 </td>
                 <td class="py-4 px-6">
-                    Yes
+                    {/* {x.room_price} */}
                 </td>
                 <td class="py-4 px-6">
-                    Yes
+                    {/* {x.is_available} */}
                 </td>
                 <td class="py-4 px-6">
-                    $2999
+                    {/* {x.hotel} */}
                 </td>
                 <td class="py-4 px-6">
-                <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
-                    
+                {/* <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
+                     */}
+                     {/* {x.room_description} */}
                 </td>
                 <td class="flex items-center py-4 px-6 space-x-3">
                     <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
@@ -83,8 +181,9 @@ export default function RoomsListComp() {
             
             
             </tr>
-
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            </div>
+})}
+            {/* <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                 <td class="p-4 w-4">
                     <div class="flex items-center">
                         <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
@@ -122,11 +221,14 @@ export default function RoomsListComp() {
                 </td>
             
             
-            </tr>
+            </tr> */}
         </tbody>
     </table>
 </div>
 
     </div>
+    
   )
+  
 }
+
